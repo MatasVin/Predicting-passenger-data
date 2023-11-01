5f56927513370fe7a231516eccc73b37c6f31367
Kimberly <149611769+KimmieCode@users.noreply.github.com>
2023-11-01 19:50:36 +0100

    Update RuterPredictions.py

--- RuterPredictions.py
+++ RuterPredictions.py
@@ -1,8 +1,11 @@
 # Import necessary libraries
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+
+
+#Yes, we also like to sort imports based on length :)
 
 # Load data
 data = pd.read_csv("Ruter_data.csv", sep=";")
